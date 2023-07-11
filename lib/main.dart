75540b2f1f95b961772304b6aa29767d66f39170
Prateek Patel <e21cseu0489@bennett.edu.in>
2023-07-12 01:17:27 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,9 @@ class PortfolioScreen extends StatelessWidget {
   final String title = "Passionate Android & Web Developer";
   final String about =
       "SDE Intern at VISANKA TECHNOLOGIES PVT LTD | Android Developer | Cross Platform App Developer | AI(open AI)| Full Stack Developer | SOLANA HACKDAY HACKATHON IIIT DELHI first prize winner✅ | HACKACCINO first prize winner✅";
+  final String email = "prateek@example.com";
+  final String linkedIn = "https://www.linkedin.com/in/prateekpatel";
+  final String github = "https://github.com/prateekpatel20";
 
   final List<Map<String, dynamic>> projects = [
     {
@@ -99,6 +102,40 @@ class PortfolioScreen extends StatelessWidget {
                     },
                   ),
                 );
+              },
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Contact Me',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            ListTile(
+              leading: Icon(Icons.mail),
+              title: Text('Email'),
+              subtitle: Text(email),
+              onTap: () async {
+                final Uri _emailLaunchUri = Uri(
+                  scheme: 'mailto',
+                  path: email,
+                );
+                await launch(_emailLaunchUri.toString());
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.link),
+              title: Text('LinkedIn'),
+              subtitle: Text('Connect with me on LinkedIn'),
+              onTap: () async {
+                await launch(linkedIn);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.code),
+              title: Text('GitHub'),
+              subtitle: Text('View my GitHub profile'),
+              onTap: () async {
+                await launch(github);
               },
             ),
           ],
