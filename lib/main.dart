981e9236045b5d3fcbf24781d464b8d3e3f05266
Prateek Patel <e21cseu0489@bennett.edu.in>
2023-07-12 13:11:19 +0530

    updating

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 void main() {
   runApp(PortfolioApp());
@@ -56,137 +57,143 @@ class PortfolioScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+        appBar: AppBar(
         title: const Text('Portfolio'),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const SizedBox(height: 20),
-            Container(
-              width: 160,
-              height: 160,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                image: DecorationImage(
-                  image: AssetImage('assets/images/profile_image.jpg'),
-                  fit: BoxFit.cover,
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black26,
-                    blurRadius: 10,
-                    offset: Offset(0, 4),
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 20),
-            Text(
-              name,
-              style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 8),
-            Text(
-              title,
-              style: const TextStyle(fontSize: 18, color: Colors.grey),
-            ),
-            const SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              child: Text(
-                about,
-                textAlign: TextAlign.center,
-                style: const TextStyle(fontSize: 16),
-              ),
-            ),
-            const SizedBox(height: 20),
-            const Text(
-              'Projects',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 10),
-            ListView.builder(
-              shrinkWrap: true,
-              itemCount: projects.length,
-              itemBuilder: (context, index) {
-                return Card(
-                  margin:
-                  const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                  elevation: 2,
-                  child: ListTile(
-                    title: Text(
-                      projects[index]['title'],
-                      style: const TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    subtitle: Text(projects[index]['description']),
-                    onTap: () async {
-                      if (await (projects[index]['url'])) {
-                        await (projects[index]['url']);
-                      }
-                    },
-                  ),
-                );
-              },
-            ),
-            const SizedBox(height: 20),
-            const Text(
-              'Contact Me',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 10),
-            ListTile(
-              leading: const Icon(Icons.mail),
-              title: const Text('Email'),
-              subtitle: Text(email),
-              onTap: () async {
-                final Uri _emailLaunchUri = Uri(
-                  scheme: 'mailto',
-                  path: email,
-                );
-                await (_emailLaunchUri.toString());
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.phone),
-              title: Text('Contact Number'),
-              subtitle: Text(contactNumber),
-              onTap: () async {
-                final Uri _phoneLaunchUri = Uri(
-                  scheme: 'tel',
-                  path: contactNumber,
-                );
-                await (_phoneLaunchUri.toString());
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.link),
-              title: const Text('LinkedIn'),
-              subtitle: Text('Connect with me on LinkedIn'),
-              onTap: () async {
-                await (linkedIn);
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.code),
-              title: const Text('GitHub'),
-              subtitle: const Text('View my GitHub profile'),
-              onTap: () async {
-                await (github);
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.insert_drive_file),
-              title: const Text('Resume'),
-              subtitle: const Text('View my resume'),
-              onTap: () async {
-                await (resumeLink);
-              },
-            ),
-          ],
-        ),
-      ),
+    ),
+    body: SingleChildScrollView(
+    child: Column(
+    crossAxisAlignment: CrossAxisAlignment.center,
+    children: [
+    const SizedBox(height: 20),
+    Transform(
+    alignment: Alignment.center,
+    transform: Matrix4.identity()
+    ..setEntry(3, 2, 0.001)
+    ..rotateX(-0.8),
+    child: Container(
+    width: 160,
+    height: 160,
+    decoration: BoxDecoration(
+    shape: BoxShape.circle,
+    image: DecorationImage(
+    image: AssetImage('assets/images/profile_image.jpg'),
+    fit: BoxFit.cover,
+    ),
+    boxShadow: [
+    BoxShadow(
+    color: Colors.black26,
+    blurRadius: 10,
+    offset: Offset(0, 4),
+    ),
+    ],
+    ),
+    ),
+    ),
+    const SizedBox(height: 20),
+    Text(
+    name,
+    style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+    ),
+    const SizedBox(height: 8),
+    Text(
+    title,
+    style: const TextStyle(fontSize: 18, color: Colors.grey),
+    ),
+    const SizedBox(height: 20),
+    Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 16),
+    child: Text(
+    about,
+    textAlign: TextAlign.center,
+    style: const TextStyle(fontSize: 16),
+    ),
+    ),
+    const SizedBox(height: 20),
+    const Text(
+    'Projects',
+    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+    ),
+    const SizedBox(height: 10),
+    ListView.builder(
+    shrinkWrap: true,
+    itemCount: projects.length,
+    itemBuilder: (context, index) {
+    return Card(
+    margin:
+    const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+    elevation: 2,
+    child: ListTile(
+    title: Text(
+    projects[index]['title'],
+    style: const TextStyle(fontWeight: FontWeight.bold),
+    ),
+    subtitle: Text(projects[index]['description']),
+    onTap: () async {
+    if (await (projects[index]['url'])) {
+    await (projects[index]['url']);
+    }
+    },
+    ),
+    );
+    },
+    ),
+    const SizedBox(height: 20),
+    const Text(
+    'Contact Me',
+    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+    ),
+    const SizedBox(height: 10),
+    ListTile(
+    leading: const Icon(Icons.mail),
+    title: const Text('Email'),
+    subtitle: Text(email),
+    onTap: () async {
+    final Uri _emailLaunchUri = Uri(
+    scheme: 'mailto',
+    path: email,
+    );
+    await (_emailLaunchUri.toString());
+    },
+    ),
+    ListTile(
+    leading: const Icon(Icons.phone),
+    title: Text('Contact Number'),
+    subtitle: Text(contactNumber),
+    onTap: () async {
+    final Uri _phoneLaunchUri = Uri(
+    scheme: 'tel',
+    path: contactNumber,
+    );
+    await (_phoneLaunchUri.toString());
+    },
+    ),
+    ListTile(
+    leading: const Icon(Icons.link),
+    title: const Text('LinkedIn'),
+    subtitle: Text('Connect with me on LinkedIn'),
+    onTap: () async {
+    await (linkedIn);
+    },
+    ),
+    ListTile(
+    leading: const Icon(Icons.code),
+    title: const Text('GitHub'),
+    subtitle: const Text('View my GitHub profile'),
+    onTap: () async {
+    await (github);
+    },
+    ),
+    ListTile(
+    leading: const Icon(Icons.insert_drive_file),
+    title: const Text('Resume'),
+    subtitle: const Text('View my resume'),
+      onTap: () async {
+        await (resumeLink);
+      },
+    ),
+    ],
+    ),
+    ),
     );
   }
 }
